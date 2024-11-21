d74c0f865b0c5d2046113c93e24155c2d530eee0
AlejandroMurilloQu <alejandro.murilloqu@gmail.com>
2024-11-21 14:23:06 -0700

    Add init files

---
+++ src/main/java/com/alura/literAlura/LiterAluraApplication.java
@@ -0,0 +1,20 @@
+package com.alura.literAlura;
+
+import com.alura.literAlura.menu.Menu;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Menu menu = new Menu();
+		menu.ShowMenu();
+	}
+}
---
+++ src/main/java/com/alura/literAlura/menu/Menu.java
@@ -0,0 +1,7 @@
+package com.alura.literAlura.menu;
+
+public class Menu {
+    public void ShowMenu(){
+
+    }
+}
